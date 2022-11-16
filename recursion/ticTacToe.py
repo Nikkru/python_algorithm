2770414e3f497539e65d22aa046a73247aae9b8a
NikNik <nickkru@gmail.com>
2022-11-16 15:45:28 +0300

    add Tic Tac Toe init

---
+++ recursion/ticTacToe.py
@@ -0,0 +1,28 @@
+class Player:
+    """
+    Класс игрока с типом значков и именем
+    """
+    pass
+
+
+
+class GameRoundManager:
+    """
+    Менеджер игры, запускающий все процессы
+    """
+    pass
+
+
+
+class GameField:
+    pass
+
+
+
+class GameFieldView:
+    pass
+
+
+
+class GameWindow:
+    pass
